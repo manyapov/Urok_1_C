c9e381bb7bcd30285f6e4bca737b0bb69bbcfffe
Mikl <m@univers.su>
2022-06-15 23:03:12 +0700

    Написали sum

--- Example002_Hellol_User/Program.cs
+++ Example002_Hellol_User/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write ("Напишите как вас Зовут?"); // Спрашиваем как зовут/ Тоесть выводим строку с вопросом
+string username = Console.ReadLine (); // помещаем текст в контенер Username
+Console.Write ("ПРИВЕТУЛЬКИ");
+ Console.Write (username);
+ // добовляем папку в корень папки example папку  .gitignore
+ // вносим в git add and commit
+// Папка нназывалась Example002_Hellol_User
+ 
+ // Write();  вывод в одну строку 
+ // write Line (); в конце перейти на новую строку 
+ // ReadLine (); считать строку в терминале 
