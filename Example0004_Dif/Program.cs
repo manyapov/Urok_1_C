8b021de63325587648d2b3b33fa8e7cb604fc358
Mikl <m@univers.su>
2022-06-15 23:45:02 +0700

    рамдомное число

--- Example0004_Dif/Program.cs
+++ Example0004_Dif/Program.cs
@@ -2,3 +2,12 @@
 double numbersA = 13;
 double numbersB = 5;
 Console.WriteLine(numbersA / numbersB);
+// Типы данных 
+// int -  от -2147 483 648
+// double -  от 
+// string-  от 0 символов . от 4 байт дл 2 гигов
+// bool  - true и false (истина или лож )1 байт 
+//  % - Нахождение остатка от деления 
+// () - менять приоритет операций 
+// генератор случайных чисел new Random().Next(min,max)
+// Даст случайные число от min до  max-1 [min,max-1] или [min, max)
--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = 10;
-int numberF = 7; 
+int numberA = new Random().Next(-1, 10); // рандомное число от 1 до 10  
+Console.WriteLine(numberA); 
+int numberF = new Random().Next(-1, 10); 
+Console.WriteLine(numberF);
 int resuil = numberA + numberF;
 Console.WriteLine(resuil); 
